811cc5c3c58719a557e47071fa1fa3729f80b21e
aravinthpm77 <110657801+aravinthpm77@users.noreply.github.com>
2023-04-02 10:06:02 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+let firebaseConfig = 
+{
+    
+    apiKey: "AIzaSyDi5IV6Im7rlVTAmPnkZknUQBjT_OYa69U",
+    authDomain: "blogging-webpage-94e7e.firebaseapp.com",
+    projectId: "blogging-webpage-94e7e",
+    storageBucket: "blogging-webpage-94e7e.appspot.com",
+    messagingSenderId: "130524553547",
+    appId: "1:130524553547:web:cd7adf4ee3b27bd7ca48ce"
+    
+      
+      
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
